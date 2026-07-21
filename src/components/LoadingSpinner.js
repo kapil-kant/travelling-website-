94c777f16d8a2df80a3f6598fe207ace24152b72
QL-Kapil-Kant <kapil.kant@quokkalabs.com>
2026-07-21 18:21:32 +0530

    Implement solution for issue #24

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner-container">
+      <div className="spinner">
+        <div className="spinner-dot"></div>
+        <div className="spinner-dot"></div>
+        <div className="spinner-dot"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
